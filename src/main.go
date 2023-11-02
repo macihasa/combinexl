3cfa9e88abb3fde8dfed44415f5b9e1945d21d32
macihasa <macihasa@gmail.com>
2023-11-02 17:44:35 +0100

    Added some command line flags with options to specify sheetname, outputname etc.

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"C"
 	"bufio"
 	"encoding/csv"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -13,91 +15,107 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-const MAX_NUM_GOROUTINES = 8
-
 // This script combines the first sheet of different Excel files into a consolidated Output.csv file.
 func main() {
-	root := promptuserforpath("Enter path to directory: ")
+	// Flags
+	var maxNumGoroutines = 8
+	var sheetName, outputName, folderPath string
+
+	flag.IntVar(&maxNumGoroutines, "g", 8, "Amount of goroutines reading files at once")
+	flag.StringVar(&sheetName, "sn", "", "Name of the specific sheets to parse.")
+	flag.StringVar(&outputName, "o", "Output", "Name of output file.")
+	flag.StringVar(&folderPath, "p", "", "Path to directory containing excel files to parse.")
+
+	flag.Parse()
+
+	if folderPath == "" {
+		folderPath = promptuserforpath("Enter path to directory: ")
+	}
+
+	fmt.Printf("\n----Variables----\nmaxNumGoroutines:\t%v\nsheetName:\t\t%v\noutputName:\t\t%v\nfolderPath:\t\t%v\n-----------------\n\n", maxNumGoroutines, sheetName, outputName, folderPath)
 
 	rowsch := make(chan []string, 1024)
 
 	readwg := new(sync.WaitGroup)
 	writewg := new(sync.WaitGroup)
 
-	routineLimiter := make(chan int, MAX_NUM_GOROUTINES)
+	routineLimiter := make(chan int, maxNumGoroutines)
 
-	go fileWriter(rowsch, root, writewg)
+	writewg.Add(1)
+	go fileWriter(rowsch, folderPath, outputName, writewg)
 
 	var filecount int
 
-	filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+	filepath.WalkDir(folderPath, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
-			log.Println(err)
+			fmt.Println(err)
 			return err
 		}
 		if d.IsDir() {
 			return nil
 		}
 		// Check if file is .xlsx or .xlsm
-		if filepath.Ext(path) == ".xlsx" || filepath.Ext(path) == ".xlsm" {
-
-		} else {
-			log.Println("Skipping file:", d.Name(), " extension:", filepath.Ext(path))
+		if filepath.Ext(path) != ".xlsx" && filepath.Ext(path) != ".xlsm" {
+			fmt.Println("Skipping file:", "["+d.Name()+"]", " extension:", filepath.Ext(path))
 			return nil
 		}
 
 		filecount++
 
-		if filecount%100 == 0 {
-			log.Println(filecount)
-		}
 		readwg.Add(1)
 		routineLimiter <- 1
-		go fileReader(path, rowsch, readwg, routineLimiter)
+		go fileReader(path, sheetName, rowsch, readwg, routineLimiter)
 
 		return nil
 	})
 	readwg.Wait()
 	close(rowsch)
 	writewg.Wait()
-	_ = promptuserforpath("Press enter to continue")
-
 }
 
-func fileReader(filename string, ch chan<- []string, wg *sync.WaitGroup, limiter chan int) {
+func fileReader(filename string, sheetName string, ch chan<- []string, wg *sync.WaitGroup, limiter chan int) {
 	f, err := excelize.OpenFile(filename)
 	if err != nil {
-		log.Println("Unable to open file: ", err)
+		fmt.Println("Unable to open file: ", err)
 	}
 
 	sheets := f.GetSheetMap()
 
-	rows, err := f.Rows(sheets[1])
+	if sheetName == "" {
+		sheetName = sheets[1]
+	} else if !checkIfSheetExists(sheetName, sheets) {
+		wg.Done()
+		fmt.Printf("Unable to find sheet: [%v]\tin file: [%v]\t\tSkipping file..\n", sheetName, filepath.Base(filename))
+		return
+	}
+
+	rows, err := f.Rows(sheetName)
 	if err != nil {
-		log.Println("not able to process row: ", err)
+		fmt.Println("unable to get rows from sheet: ", err)
 	}
 
 	for rows.Next() {
 		row, err := rows.Columns()
 		if err != nil {
-			log.Println("unable to get row columns, ", err)
+			fmt.Println("unable to get row columns, ", err)
 		}
 		ch <- row
 	}
 	err = rows.Close()
 	if err != nil {
-		log.Println("unable to close file: ", err)
+		fmt.Println("unable to close file: ", err)
 	}
 	<-limiter
 	wg.Done()
 }
 
-func fileWriter(ch <-chan []string, path string, wg *sync.WaitGroup) {
+// Filewriter creates and writes a csv file from the rows channel.
+func fileWriter(ch <-chan []string, path string, fileName string, wg *sync.WaitGroup) {
 	// Make sure path has a trailing slash and create file
 	if path[len(path)-1:] != "/" {
 		path = path + "/"
 	}
-	f, err := os.Create(path + "Output " + time.Now().Format("2006-01-02 15_04_05") + ".csv")
+	f, err := os.Create(path + fileName + " " + time.Now().Format("2006-01-02 15_04_05") + ".csv")
 	if err != nil {
 		log.Fatal("Unable to create file: ", err)
 	}
@@ -106,26 +124,36 @@ func fileWriter(ch <-chan []string, path string, wg *sync.WaitGroup) {
 	// initialize csv writer
 	writer := csv.NewWriter(f)
 	writer.Comma = rune(';')
-	rowCount := 1
+	rowCount := 0
 
 	// write rows to file
 	for row := range ch {
 		if rowCount%1000 == 0 {
-			log.Println("Rows processed: ", rowCount)
+			fmt.Println("Rows processed: ", rowCount)
 			writer.Flush()
 		}
 		writer.Write(row)
 		rowCount++
 	}
-	log.Println("Rows processed: ", rowCount)
+	fmt.Println("Rows processed: ", rowCount)
 	writer.Flush()
 	wg.Done()
 }
 
+func checkIfSheetExists(sheetName string, sheets map[int]string) bool {
+	for _, v := range sheets {
+		if v == sheetName {
+			return true
+		}
+	}
+	return false
+}
+
+// promptuserforpath is used when user excluded the -p flag. For the script to run a path is required.
 func promptuserforpath(prompt string) string {
 	var userInput string
 	scanner := bufio.NewScanner(os.Stdin)
-	log.Print(prompt)
+	fmt.Print(prompt)
 	scanner.Scan()
 	userInput = scanner.Text()
 	return userInput
