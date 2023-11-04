daedc349f4b6fb553d8a3c7ec2dc26f39bd14309
macihasa <macihasa@gmail.com>
2023-11-04 15:03:33 +0100

    Restructured code and created a [Config] type. The config is used to pass all flags to the different functions.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"C"
 	"bufio"
 	"encoding/csv"
 	"flag"
@@ -9,76 +8,108 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"reflect"
 	"sync"
 	"time"
 
 	"github.com/xuri/excelize/v2"
 )
 
+type Config struct {
+	MaxNumReaders int
+	SheetName     string
+	OutputName    string
+	FolderPath    string
+	Delimiter     rune
+	Recursive     bool
+}
+
 func main() {
 	// Flags and setup
-	var maxNumGoroutines int
-	var sheetName, outputName, folderPath, delimiterString string
-	var csvDelimiter rune
-	var recursive bool
+	config := parseFlags()
+	printFlags(config)
 
-	flag.IntVar(&maxNumGoroutines, "g", 8, "Amount of goroutines reading files at once")
-	flag.StringVar(&sheetName, "sn", "", "Name of the specific sheets to parse. (Optional)")
-	flag.StringVar(&outputName, "o", "Output", "Name of output file.")
-	flag.StringVar(&folderPath, "p", "", "Path to directory containing excel files to parse.")
-	flag.StringVar(&delimiterString, "d", ";", "Csv delimiter for the output file. Maximum 1 letter.")
-	flag.BoolVar(&recursive, "r", false, "Recursively goes through each subdirectory and iterates through their excel documents.")
-
-	flag.Parse()
-
-	if len(delimiterString) > 1 || len(delimiterString) == 0 {
-		fmt.Print("The csv delimiter provided through [-d] can only be 1 character long. Input provided: [" + delimiterString + "]")
-		return
-	}
-
-	for _, v := range delimiterString {
-		csvDelimiter = v
-	}
-
-	if folderPath == "" {
-		folderPath = promptuserforpath("Enter path to directory: ")
-	}
-
-	fmt.Printf("\n----Variables----\nmaxNumGoroutines:\t%v\nsheetName:\t\t%v\noutputName:\t\t%v\nfolderPath:\t\t%v\ncsvDelimiter:\t\t%s\nrecursive:\t\t%v\n-----------------\n\n", maxNumGoroutines, sheetName, outputName, folderPath, delimiterString, recursive)
-
-	// Start of processing
 	rowsch := make(chan []string, 1024)
 
 	readwg := new(sync.WaitGroup)
 	writewg := new(sync.WaitGroup)
 
-	routineLimiter := make(chan int, maxNumGoroutines)
+	routineLimiter := make(chan int, config.MaxNumReaders)
 
+	// Launch filewriter routine
 	writewg.Add(1)
-	go fileWriter(rowsch, folderPath, outputName, csvDelimiter, writewg)
+	go fileWriter(rowsch, config, writewg)
 
-	iterateFolder(folderPath, sheetName, readwg, rowsch, routineLimiter, recursive)
+	// Launch fileReader routines
+	iterateFolder(config, readwg, routineLimiter, rowsch)
 
 	readwg.Wait()
 	close(rowsch)
 	writewg.Wait()
 }
 
-func iterateFolder(folderPath, sheetName string, readwg *sync.WaitGroup, rowsch chan<- []string, routineLimiter chan int, recursive bool) {
-	filepath.WalkDir(folderPath, func(path string, d os.DirEntry, err error) error {
+// parseFlags parses command-line flags and returns a Config with user-specified options.
+// It handles options for concurrent readers, sheet name, output file, folder path, CSV delimiter, and recursion.
+// Invalid flags or missing folder path terminate the program.
+func parseFlags() Config {
+	var config Config
+	var delimiterString string
+	flag.IntVar(&config.MaxNumReaders, "g", 8, "Limits the amount of files being read at once")
+	flag.StringVar(&config.SheetName, "sn", "", "Name of the specific sheets to parse. (Optional)")
+	flag.StringVar(&config.OutputName, "o", "Output", "Name of output file.")
+	flag.StringVar(&config.FolderPath, "p", "", "Path to directory containing excel files to parse.")
+	flag.StringVar(&delimiterString, "d", ";", "Csv delimiter for the output file. Maximum 1 letter.")
+	flag.BoolVar(&config.Recursive, "r", false, "Recursively goes through each subdirectory and iterates through their excel documents.")
+
+	flag.Parse()
+
+	if len(delimiterString) > 1 || len(delimiterString) == 0 {
+		log.Fatal("The csv delimiter provided through [-d] can only be 1 character long. Input provided: [" + delimiterString + "]")
+	}
+
+	for _, v := range delimiterString {
+		config.Delimiter = v
+	}
+	if config.FolderPath == "" {
+		config.FolderPath = promptuserforpath("Enter path to directory: ")
+	}
+	if config.FolderPath == "" {
+		log.Fatal("A folder path to a directory is mandatory.")
+	}
+
+	return config
+}
+
+// printFlags prints the values of the provided Config struct's fields with their corresponding field names.
+func printFlags(config Config) {
+	v := reflect.ValueOf(config)
+	fmt.Println("----Variables----")
+	for i := 0; i < v.NumField(); i++ {
+		// Print field name and value
+		fmt.Println(v.Type().Field(i).Name, "\t\t", v.Field(i).Interface())
+	}
+	fmt.Printf("-----------------\n\n")
+}
+
+// iterateFolder walks the folder provided in [folderPath] and launches a filereader routine for each excel file it finds.
+// If the recursive flag is provided [-r] it also calls itself upon all subdirectories of [folderPath].
+func iterateFolder(c Config, readwg *sync.WaitGroup, routineLimiter chan int, rowsch chan<- []string) {
+	filepath.WalkDir(c.FolderPath, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
 			fmt.Println(err)
 			return err
 		}
 
 		// Recursive call if user want to read files in subdirectories
-		if d.IsDir() && recursive && folderPath != path {
-			iterateFolder(path, sheetName, readwg, rowsch, routineLimiter, recursive)
+		if d.IsDir() && c.Recursive && c.FolderPath != path {
+			newConfig := c
+			newConfig.FolderPath = path
+			iterateFolder(newConfig, readwg, routineLimiter, rowsch)
 			return nil
 		}
 
 		// Make sure we're in the folderpath provided by the function caller.
-		if filepath.Dir(path) != folderPath {
+		if filepath.Dir(path) != c.FolderPath {
 			return nil
 		}
 
@@ -90,18 +121,20 @@ func iterateFolder(folderPath, sheetName string, readwg *sync.WaitGroup, rowsch 
 
 		readwg.Add(1)
 		routineLimiter <- 1
-		go fileReader(path, sheetName, rowsch, readwg, routineLimiter)
+		go fileReader(path, c.SheetName, rowsch, readwg, routineLimiter)
 		return nil
 	})
 }
 
-// fileReader opens and parses the rows of an excel document sheet into the channel ch.
-// If the user does not provide a specific sheet name through [-sn], the first sheet of each file is processed instead.
+// fileReader opens and parses the rows of an Excel document sheet into the channel [ch].
+// If a specific sheet name is not provided, it processes the first sheet of each file.
+// The amount of concurrent fileReaders is limited by the channel [limiter].
 func fileReader(filename string, sheetName string, ch chan<- []string, wg *sync.WaitGroup, limiter chan int) {
 	f, err := excelize.OpenFile(filename)
 	if err != nil {
 		fmt.Println("Unable to open file: ", err)
 	}
+	defer f.Close()
 
 	sheets := f.GetSheetMap()
 
@@ -134,13 +167,14 @@ func fileReader(filename string, sheetName string, ch chan<- []string, wg *sync.
 	wg.Done()
 }
 
-// Filewriter creates and writes a csv file from the rows channel ch.
-func fileWriter(ch <-chan []string, path string, fileName string, delimiter rune, wg *sync.WaitGroup) {
+// fileWriter creates and writes a CSV file from the rows received on the input channel [ch].
+// The output file is named based on the provided configuration and the current timestamp.
+func fileWriter(ch <-chan []string, c Config, wg *sync.WaitGroup) {
 	// Make sure path has a trailing slash and create file
-	if path[len(path)-1:] != "/" {
-		path = path + "/"
+	if c.FolderPath[len(c.FolderPath)-1:] != "/" {
+		c.FolderPath = c.FolderPath + "/"
 	}
-	f, err := os.Create(path + fileName + " " + time.Now().Format("2006-01-02 15_04_05") + ".csv")
+	f, err := os.Create(c.FolderPath + c.OutputName + " " + time.Now().Format("2006-01-02 15_04_05") + ".csv")
 	if err != nil {
 		log.Fatal("Unable to create file: ", err)
 	}
@@ -148,7 +182,7 @@ func fileWriter(ch <-chan []string, path string, fileName string, delimiter rune
 
 	// initialize csv writer
 	writer := csv.NewWriter(f)
-	writer.Comma = delimiter
+	writer.Comma = c.Delimiter
 	rowCount := 0
 
 	// write rows to file
