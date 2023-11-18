33d3462b2c97eff5d02fbf8a1a8b246606a97b26
macihasa <macihasa@gmail.com>
2023-11-18 18:56:25 +0100

    Created worker pool instead of launching a new filereader goroutine for each file. closes #4

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"C"
 	"bufio"
 	"encoding/csv"
 	"flag"
@@ -16,6 +15,7 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+// Config contains the flag properties provided by the user.
 type Config struct {
 	MaxNumReaders int
 	SheetName     string
@@ -36,18 +36,163 @@ func main() {
 	readwg := new(sync.WaitGroup)
 	writewg := new(sync.WaitGroup)
 
-	routineLimiter := make(chan int, config.MaxNumReaders)
-
 	// Launch filewriter routine
 	writewg.Add(1)
 	go fileWriter(rowsch, config, writewg)
 
-	// Launch fileReader routines
-	iterateFolder(config, readwg, routineLimiter, rowsch)
+	// Retrieve filepaths/names
+	filenames := []string{}
+	iterateFolder(config, &filenames)
+
+	// Add filenames to filenames channel and close it when done
+	filenamesch := make(chan string, len(filenames))
+	for _, v := range filenames {
+		filenamesch <- v
+	}
+	close(filenamesch)
+
+	// Launch filereader routines
+	if len(filenames) < config.MaxNumReaders {
+		for i := 0; i < len(filenames); i++ {
+			readwg.Add(1)
+			go fileReader(filenamesch, config, rowsch, readwg)
+		}
+	} else {
+		for i := 0; i < config.MaxNumReaders; i++ {
+			readwg.Add(1)
+			go fileReader(filenamesch, config, rowsch, readwg)
+		}
+	}
 
 	readwg.Wait()
 	close(rowsch)
 	writewg.Wait()
+}
+
+// iterateFolder walks the folder provided in [folderPath] and adds all .
+// If the recursive flag is provided [-r] it also calls itself upon all subdirectories of [folderPath].
+func iterateFolder(c Config, filesToRead *[]string) {
+	filepath.WalkDir(c.FolderPath, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		// Recursive call if user want to read files in subdirectories
+		if d.IsDir() && c.Recursive && c.FolderPath != path {
+			newConfig := c
+			newConfig.FolderPath = path
+			iterateFolder(newConfig, filesToRead)
+			return nil
+		}
+
+		// Make sure we're in the folderpath provided by the function caller.
+		if filepath.Dir(path) != c.FolderPath {
+			return nil
+		}
+
+		// Check if file is .xlsx or .xlsm
+		if filepath.Ext(path) != ".xlsx" && filepath.Ext(path) != ".xlsm" {
+			if d.IsDir() {
+				fmt.Println("Skipping directory:", "["+d.Name()+"]")
+				return nil
+			}
+			fmt.Println("Skipping file:", "["+d.Name()+"]", " extension:", filepath.Ext(path))
+			return nil
+		}
+
+		// Check if startswith flag is populated and if the file name starts with that string
+		if c.StartsWith != "" && d.Name()[:len(c.StartsWith)] != c.StartsWith {
+			fmt.Println("Skipping file:", "["+d.Name()+"]", "does not start with: ["+c.StartsWith+"]")
+			return nil
+		}
+
+		// Add filepath to filestoread.
+		*filesToRead = append(*filesToRead, path)
+		return nil
+	})
+}
+
+// fileReaders opens and parses the rows of an Excel document which location is provided by the [filenames] channel.
+// It sends the rows into the rows channel [ch] for the filewriter.
+// If a specific sheet name is not provided, it processes the first sheet of each file.
+func fileReader(filenames chan string, c Config, ch chan<- []string, wg *sync.WaitGroup) {
+	for v := range filenames {
+		f, err := excelize.OpenFile(v)
+		if err != nil {
+			fmt.Println("Unable to open file: ", err)
+		}
+		sheets := f.GetSheetMap()
+		sheetName := c.SheetName
+
+		// Check if sheetName is populated. If not set it to the first sheet in the file
+		if sheetName == "" {
+			sheetName = sheets[1]
+		} else if !checkIfSheetExists(sheetName, sheets) {
+			fmt.Printf("Unable to find sheet: [%v]\tin file: [%v]\t\tSkipping file..\n", sheetName, filepath.Base(v))
+			continue
+		}
+		// Get rows iterator from sheet
+		rows, err := f.Rows(sheetName)
+		if err != nil {
+			fmt.Println("unable to get rows from sheet: ", err)
+			continue
+		}
+
+		// Iterate over rows and send them to the rows channel for the writer to write
+		for rows.Next() {
+			row, err := rows.Columns()
+			if err != nil {
+				fmt.Println("unable to get row columns, ", err)
+			}
+			ch <- row
+		}
+		err = rows.Close()
+		if err != nil {
+			fmt.Println("unable to close rows iterator: ", err)
+			continue
+		}
+
+		err = f.Close()
+		if err != nil {
+			fmt.Println("unable to close file", err)
+			continue
+		}
+	}
+	wg.Done()
+}
+
+// fileWriter creates and writes a CSV file from the rows received on the input channel [ch].
+// The output file is named based on the provided configuration and the current timestamp.
+func fileWriter(ch <-chan []string, c Config, wg *sync.WaitGroup) {
+	// Make sure path has a trailing slash and create file
+	outputFileName := c.OutputName + " " + time.Now().Format("2006-01-02 15_04_05") + ".csv"
+	outputFilePath := filepath.Join(c.FolderPath, outputFileName)
+
+	f, err := os.Create(outputFilePath)
+
+	if err != nil {
+		log.Fatal("Unable to create file: ", err)
+	}
+	defer f.Close()
+
+	// initialize csv writer
+	writer := csv.NewWriter(f)
+	writer.Comma = c.Delimiter
+	rowCount := 0
+
+	// write rows to file
+	for row := range ch {
+		if rowCount%1000 == 0 {
+			fmt.Println("Rows processed: ", rowCount)
+			writer.Flush()
+		}
+		writer.Write(row)
+		rowCount++
+	}
+	fmt.Println("Rows processed: ", rowCount)
+	writer.Flush()
+	wg.Done()
 }
 
 // parseFlags parses command-line flags and returns a Config with user-specified options.
@@ -92,126 +237,6 @@ func printFlags(config Config) {
 		fmt.Println(v.Type().Field(i).Name, "\t\t", v.Field(i).Interface())
 	}
 	fmt.Printf("-----------------\n\n")
-}
-
-// iterateFolder walks the folder provided in [folderPath] and launches a filereader routine for each excel file it finds.
-// If the recursive flag is provided [-r] it also calls itself upon all subdirectories of [folderPath].
-func iterateFolder(c Config, readwg *sync.WaitGroup, routineLimiter chan int, rowsch chan<- []string) {
-	filepath.WalkDir(c.FolderPath, func(path string, d os.DirEntry, err error) error {
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-		// Recursive call if user want to read files in subdirectories
-		if d.IsDir() && c.Recursive && c.FolderPath != path {
-			newConfig := c
-			newConfig.FolderPath = path
-			iterateFolder(newConfig, readwg, routineLimiter, rowsch)
-			return nil
-		}
-
-		// Make sure we're in the folderpath provided by the function caller.
-		if filepath.Dir(path) != c.FolderPath {
-			return nil
-		}
-
-		// Check if file is .xlsx or .xlsm
-		if filepath.Ext(path) != ".xlsx" && filepath.Ext(path) != ".xlsm" {
-			if d.IsDir() {
-				fmt.Println("Skipping directory:", "["+d.Name()+"]")
-				return nil
-			}
-			fmt.Println("Skipping file:", "["+d.Name()+"]", " extension:", filepath.Ext(path))
-			return nil
-		}
-
-		// Check if startswith is populated and if the file name starts with that string
-		if c.StartsWith != "" && d.Name()[:len(c.StartsWith)] != c.StartsWith {
-			fmt.Println("Skipping file:", "["+d.Name()+"]", "does not start with: ["+c.StartsWith+"]")
-			return nil
-		}
-
-		readwg.Add(1)
-		routineLimiter <- 1
-		go fileReader(path, c.SheetName, rowsch, readwg, routineLimiter)
-		return nil
-	})
-}
-
-// fileReader opens and parses the rows of an Excel document sheet into the channel [ch].
-// If a specific sheet name is not provided, it processes the first sheet of each file.
-// The amount of concurrent fileReaders running at one time is limited by the channel [limiter].
-func fileReader(filename string, sheetName string, ch chan<- []string, wg *sync.WaitGroup, limiter chan int) {
-	f, err := excelize.OpenFile(filename)
-	if err != nil {
-		fmt.Println("Unable to open file: ", err)
-	}
-	defer f.Close()
-
-	sheets := f.GetSheetMap()
-
-	// Check if sheetName is populated. If not set it to the first sheet in the file
-	if sheetName == "" {
-		sheetName = sheets[1]
-	} else if !checkIfSheetExists(sheetName, sheets) {
-		wg.Done()
-		fmt.Printf("Unable to find sheet: [%v]\tin file: [%v]\t\tSkipping file..\n", sheetName, filepath.Base(filename))
-		return
-	}
-
-	// Get rows from sheet
-	rows, err := f.Rows(sheetName)
-	if err != nil {
-		fmt.Println("unable to get rows from sheet: ", err)
-	}
-
-	// Iterate over rows and send them to the channel
-	for rows.Next() {
-		row, err := rows.Columns()
-		if err != nil {
-			fmt.Println("unable to get row columns, ", err)
-		}
-		ch <- row
-	}
-	err = rows.Close()
-	if err != nil {
-		fmt.Println("unable to close file: ", err)
-	}
-	<-limiter
-	wg.Done()
-}
-
-// fileWriter creates and writes a CSV file from the rows received on the input channel [ch].
-// The output file is named based on the provided configuration and the current timestamp.
-func fileWriter(ch <-chan []string, c Config, wg *sync.WaitGroup) {
-	// Make sure path has a trailing slash and create file
-	if c.FolderPath[len(c.FolderPath)-1:] != "/" {
-		c.FolderPath = c.FolderPath + "/"
-	}
-	f, err := os.Create(c.FolderPath + c.OutputName + " " + time.Now().Format("2006-01-02 15_04_05") + ".csv")
-	if err != nil {
-		log.Fatal("Unable to create file: ", err)
-	}
-	defer f.Close()
-
-	// initialize csv writer
-	writer := csv.NewWriter(f)
-	writer.Comma = c.Delimiter
-	rowCount := 0
-
-	// write rows to file
-	for row := range ch {
-		if rowCount%1000 == 0 {
-			fmt.Println("Rows processed: ", rowCount)
-			writer.Flush()
-		}
-		writer.Write(row)
-		rowCount++
-	}
-	fmt.Println("Rows processed: ", rowCount)
-	writer.Flush()
-	wg.Done()
 }
 
 // checkIfSheetExists takes a sheetname and checks if that exists within a map of sheets.
